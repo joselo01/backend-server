7c4814d18031f46f21438326332d07a3687e7d3a
joselo01 <sjmujica@gmail.com>
2021-10-05 11:31:37 -0500

    update server

--- database/config.js
+++ database/config.js
@@ -3,8 +3,10 @@ const mongoose = require("mongoose");
 const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.DB_CNN, {
+      useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useFindAndModify: false,
     });
 
     console.log("DB Online");
