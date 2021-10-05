97c0b24622e0b72f91b093b2e7ec14601be25733
joselo01 <sjmujica@gmail.com>
2021-10-05 01:33:58 -0500

    Backend 1.0 terminado

---
+++ README.md
@@ -0,0 +1,5 @@
+# Backend USUARIO - PP-GG
+
+Backend creado para puebas de APIS: Crado por Sixto Mujica
+
+sjmujica@gmail.com
---
+++ controllers/auth.js
@@ -0,0 +1,49 @@
+const { response } = require("express");
+const bcrypt = require("bcryptjs");
+
+const Usuario = require("../models/usuarios");
+const { generarJWT } = require("../helpers/jwt");
+
+const login = async (req, res = response) => {
+  const { email, password } = req.body;
+
+  try {
+    const usuarioBD = await Usuario.findOne({
+      email,
+    });
+
+    if (!usuarioBD) {
+      return res.status(404).json({
+        ok: false,
+        msg: "email no encontrado",
+      });
+    }
+
+    const validPassword = bcrypt.compareSync(password, usuarioBD.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Contraseña no valida",
+      });
+    }
+
+    const token = await generarJWT(usuarioBD.id);
+
+
+    res.json({
+      ok: true,
+      token
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
+};
+
+module.exports = {
+  login,
+};
